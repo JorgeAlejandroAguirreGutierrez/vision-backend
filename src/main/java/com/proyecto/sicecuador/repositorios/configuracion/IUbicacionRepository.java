c7799dd5adf6e2576165ea7902afb9561e47efd9
Jorge Hidalgo <gato_sta@hotmail.com>
2023-03-28 21:25:20 -0500

    Arreglo ubicacion

--- src/main/java/com/proyecto/sicecuador/repositorios/configuracion/IUbicacionRepository.java
+++ src/main/java/com/proyecto/sicecuador/repositorios/configuracion/IUbicacionRepository.java
@@ -18,13 +18,13 @@ public interface IUbicacionRepository extends JpaRepository<Ubicacion, Long>, Jp
 	@Query(value = "select u from Ubicacion u where u.codigoNorma like '%'||:codigoNorma||'%' and u.provincia like '%'||:provincia||'%' and u.canton like '%'||:canton||'%' and u.parroquia like '%'||:parroquia||'%' order by u.codigo asc")
 	List<Ubicacion> buscar(String codigoNorma, String provincia, String canton, String parroquia);
 	
-	@Query(value = "select distinct u.provincia from ubicacion u where u.estado = :estado order by u.provincia order by u.codigo asc", nativeQuery = true)
+	@Query(value = "select distinct u.provincia from ubicacion u where u.estado = :estado order by u.provincia", nativeQuery = true)
     List<String> findProvincias(String estado);
 
-    @Query(value = "select distinct u.canton from ubicacion u where u.provincia =:provincia and u.estado= :estado order by u.canton order by u.codigo asc" ,nativeQuery = true)
+    @Query(value = "select distinct u.canton from ubicacion u where u.provincia =:provincia and u.estado= :estado order by u.canton" ,nativeQuery = true)
     List<String> findCantones(String provincia, String estado);
 
-    @Query(value = "select * from ubicacion u where u.canton = :canton and u.estado= :estado order by u.codigo asc", nativeQuery = true)
+    @Query(value = "select * from ubicacion u where u.canton = :canton and u.estado= :estado order by u.parroquia", nativeQuery = true)
     List<Ubicacion> findParroquias(String canton, String estado);
 
     @Query(value = "select u from Ubicacion u where u.provincia = :provincia and u.canton = :canton and u.parroquia = :parroquia and u.estado= :estado order by u.codigo asc")
