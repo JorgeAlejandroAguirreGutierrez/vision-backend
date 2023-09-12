0195f01b2707e8023a8995db8008a9b86d0dd4b9
Jorge Alejandro Aguirre Gutierrez <joaaguir@bancolombia.com.co>
2023-09-12 12:29:49 -0500

    mod

--- src/main/java/com/proyecto/vision/Constantes.java
+++ src/main/java/com/proyecto/vision/Constantes.java
@@ -538,8 +538,10 @@ public class Constantes {
     public static final int once= 11;
     public static final double ciencuenta = 50;
 
-    public static final String urlFacturacionEletronicaSri = "https://celcer.sri.gob.ec/comprobantes-electronicos-ws/RecepcionComprobantesOffline";
-    public static final String urlConsultaFacturacionEletronicaSri = "https://celcer.sri.gob.ec/comprobantes-electronicos-ws/AutorizacionComprobantesOffline";
+    public static final String urlPruebasFacturacionEletronicaSri = "https://celcer.sri.gob.ec/comprobantes-electronicos-ws/RecepcionComprobantesOffline";
+    public static final String urlPruebasConsultaFacturacionEletronicaSri = "https://celcer.sri.gob.ec/comprobantes-electronicos-ws/AutorizacionComprobantesOffline";
+    public static final String urlProduccionFacturacionEletronicaSri = "https://cel.sri.gob.ec/comprobantes-electronicos-ws/RecepcionComprobantesOffline";
+    public static final String urlProduccionConsultaFacturacionEletronicaSri = "https://cel.sri.gob.ec/comprobantes-electronicos-ws/AutorizacionComprobantesOffline";
     public static final String utf8 = "utf-8";
     public static final String contentType = "Content-Type";
     public static final String moneda = "USD";
