6946081f53a6a07658f3a7b35c90490cb0194001
Jorge Alejandro Aguirre Gutierrez <joaaguir@bancolombia.com.co>
2024-04-19 17:22:00 -0500

    mod

--- src/main/java/com/proyecto/vision/servicios/interf/configuracion/ISincronizacionService.java
+++ src/main/java/com/proyecto/vision/servicios/interf/configuracion/ISincronizacionService.java
@@ -3,12 +3,14 @@ package com.proyecto.vision.servicios.interf.configuracion;
 import com.proyecto.vision.modelos.configuracion.Modelo;
 import com.proyecto.vision.modelos.configuracion.Sincronizacion;
 import com.proyecto.vision.servicios.interf.IGenericoService;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface ISincronizacionService extends IGenericoService<Sincronizacion> {
 	void validar(Sincronizacion sincronizacion);
 	List<Sincronizacion> consultarPorEstado(String estado);
+	Sincronizacion cargarArchivo(long sincronizacionId, MultipartFile multipartFile);
 	List<Modelo> procesar(long sincronizacionId);
 	void crearModelos(List<Modelo> modelos);
 }
